43ebaf5115bc27c133ceb017a0b6559781270121
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2021-02-25 01:11:51 +0530

    submit form using hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
+
+
+const App = () => {
+
+
+  const [name ,setName]  = useState("");
+  const [fullName,setFullName] =useState();
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
+
+  const onSubmit=()=>{
+    setFullName(name);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {fullName}</h1>
+      <input type="text" placeholder="Enter Your Name" onChange={inputEvent}
+      value ={name}/>
+      <button onClick ={onSubmit}>Click Me </button>
     </div>
   );
-}
-
+};
 export default App;
